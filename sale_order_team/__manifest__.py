998f10ad0e34148267ecb7b04016972f9366adef
shahd111no <143444265+shahd111no@users.noreply.github.com>
2026-06-06 17:27:28 +0300

    Add files via upload

---
+++ sale_order_team/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': "sale_order_team",
+    'author': "shahd mohamed",
+    'category': "",
+    'version': "17.0.0.1.0",
+    'depends': [
+        'base','mail','contacts','sale_management'
+    ],
+    'data': [
+        'security/security.xml',
+
+
+
+
+
+    ],
+
+
+    'assets': {
+        'web.assets_backend': [
+
+
+
+        ],
+    },
+    'application': True,
+
+}
---
+++ sale_order_team/models/sale_order_team.py
@@ -0,0 +1,6 @@
+from odoo import api, fields, models, tools
+
+class SaleOrderTeam(models.Model):
+    _inherit = 'sale.order'
+
+    
